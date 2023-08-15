e10cf1134ad0ad0931403b5240638f6482e0b104
Andres <alizoandres11@gmail.com>
2023-08-14 23:47:07 -0400

    [BugFix] LogService Logic

--- src/log/log.service.ts
+++ src/log/log.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import { Log } from './schemas/log.schema';
+import { PgsqldriverService } from '../pgsqldriver/pgsqldriver.service';
 
 @Injectable()
 export class LogService {
-  constructor(@InjectModel('Logs') private logModel: Model<Log>) {}
+  constructor(private readonly pgsqlDriverService: PgsqldriverService) {}
 
-  async create(logData: Partial<Log>): Promise<Log> {
-    const log = new this.logModel(logData);
-    return log.save();
+  async create(log: Log) {
+    await this.pgsqlDriverService.create(log);
   }
+
 }
