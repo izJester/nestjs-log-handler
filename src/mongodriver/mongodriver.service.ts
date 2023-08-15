a6dd79770571e3f14397bcede50611df83b65e08
Andres <alizoandres11@gmail.com>
2023-08-15 14:11:58 -0400

    [Feature] Mongo driver service

--- src/mongodriver/mongodriver.service.ts
+++ src/mongodriver/mongodriver.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Log } from '../log/schemas/log.schema';
 
 @Injectable()
-export class MongodriverService {}
+export class MongodriverService {
+  constructor(@InjectModel('Logs') private logModel: Model<Log>) {}
+
+  async create(logData: Partial<Log>): Promise<Log> {
+    const log = new this.logModel(logData);
+    return log.save();
+  }
+
+  async findAll(): Promise<Log[]> {
+    return this.logModel.find().exec();
+  }
+}
