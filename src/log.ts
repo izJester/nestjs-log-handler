56589c9dbd34822edeed8f8ca27c65d8bd568bc9
Andres <alizoandres11@gmail.com>
2023-08-14 23:51:31 -0400

    [BugFix] Main middleware

--- src/log.ts
+++ src/log.ts
@@ -5,13 +5,13 @@ import { LogService } from './log/log.service';
 
 @Injectable()
 export class LogHandler implements NestMiddleware {
-  constructor(private readonly logsService: LogService) {}
+  constructor(private readonly logService: LogService) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
-    const logMessage = `${req.method} ${req.originalUrl} from ${req.ip}`;
+    const logMessage = {method: req.method, originalUrl: req.originalUrl, ip: req.ip};
     
     // Almacena el mensaje en la tabla de logs
-    await this.logsService.create({ content: logMessage });
+    await this.logService.create({ content: logMessage });
 
     next();
   }
